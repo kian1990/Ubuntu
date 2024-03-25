068b4924c1138d5896ac01f1b0c658d4d07f6f14
kian1990 <269590936@qq.com>
2024-03-25 17:48:36 +0800

    Add files via upload

---
+++ test/go_web/app.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "net/http"
+    "github.com/gin-gonic/gin"
+)
+
+func main() {
+    router := gin.Default()
+
+    router.LoadHTMLGlob("templates/*")
+
+    router.GET("/", func(c *gin.Context) {
+        c.HTML(http.StatusOK, "index.html", nil)
+    })
+
+    router.Static("/static", "./static")
+
+    router.Run(":80")
+}
